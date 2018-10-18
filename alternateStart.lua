9b4d30e2ddcae4d3088070ac32ca75e8c5558e5c
Boyos999 <44275954+Boyos999@users.noreply.github.com>
2018-10-18 17:08:43 -0400

    Update alternateStart.lua

--- alternateStart.lua
+++ alternateStart.lua
@@ -23,6 +23,51 @@ text = "Choose your starting location.",
 				}) 
 			}
 		},
+		{ caption = "Ald-ruhn", 
+			destinations = { 
+				menuHelper.destinations.setDefault(nil,
+				{
+					menuHelper.effects.runPlayerFunction("AltStart", {"-2,6",{-12544, 53600, 2384.217},{0, 0}}),
+					menuHelper.effects.runPlayerFunction("Message", {"Spawned in Ald-ruhn\n"})
+				}) 
+			}
+		},
+		{ caption = "Vivec", 
+			destinations = { 
+				menuHelper.destinations.setDefault(nil,
+				{
+					menuHelper.effects.runPlayerFunction("AltStart", {"Vivec, Foreign Quarter Plaza",{-64, 128, 290.927},{0, 90}}),
+					menuHelper.effects.runPlayerFunction("Message", {"Spawned in Vivec\n"})
+				}) 
+			}
+		},
+		{ caption = "Sadrith Mora", 
+			destinations = { 
+				menuHelper.destinations.setDefault(nil,
+				{
+					menuHelper.effects.runPlayerFunction("AltStart", {"17,4",{143168, 35968, 528.649},{0, 90}}),
+					menuHelper.effects.runPlayerFunction("Message", {"Spawned in Sadrith Mora\n"})
+				}) 
+			}
+		},
+		{ caption = "Gnisis", 
+			destinations = { 
+				menuHelper.destinations.setDefault(nil,
+				{
+					menuHelper.effects.runPlayerFunction("AltStart", {"-11,11",{-86144, 92064, 1036.217},{0, 0}}),
+					menuHelper.effects.runPlayerFunction("Message", {"Spawned in Gnisis\n"})
+				}) 
+			}
+		},
+		{ caption = "Suran", 
+			destinations = { 
+				menuHelper.destinations.setDefault(nil,
+				{
+					menuHelper.effects.runPlayerFunction("AltStart", {"6,-7",{53648.527, -51050.477, 224.153},{0, 90}}),
+					menuHelper.effects.runPlayerFunction("Message", {"Spawned in Suran\n"})
+				}) 
+			}
+		},
         { caption = "Exit", destinations = nil }
     }
 }
