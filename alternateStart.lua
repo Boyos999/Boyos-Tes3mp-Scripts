a568e2e85000d45d3029d7b2db977532c37c0833
Boyos999 <44275954+Boyos999@users.noreply.github.com>
2018-10-18 16:50:42 -0400

    Create alternateStart.lua

---
+++ alternateStart.lua
@@ -0,0 +1,28 @@
+--To install follow the instructions on https://github.com/Boyos999/tes3mpScripts
+
+
+
+Menus["Alternate Start"] = {
+text = "Choose your starting location.",
+    buttons = {
+        { caption = "Default (Balmora)", 
+			destinations = { 
+				menuHelper.destinations.setDefault(nil,
+				{
+					menuHelper.effects.runPlayerFunction("AltStart", {"-3,-2",{-23980.693359375, -15561.556640625, 505},{-0.000152587890625, 1.6182196140289}}),
+					menuHelper.effects.runPlayerFunction("Message", {"Spawned in Balmora\n"})
+				}) 
+			} 
+		},
+        { caption = "Seyda Neen", 
+			destinations = { 
+				menuHelper.destinations.setDefault(nil,
+				{
+					menuHelper.effects.runPlayerFunction("AltStart", {"-2,-9",{-10912, -71008, 240.122},{0, 0}}),
+					menuHelper.effects.runPlayerFunction("Message", {"Spawned in Seyda Neen\n"})
+				}) 
+			}
+		},
+        { caption = "Exit", destinations = nil }
+    }
+}
