2b2640f869065f1e0fc56e363755d1aaf1ecc044
Boyos999 <Boyos999@gmail.com>
2018-10-25 13:51:32 -0400

    Cleaning up resMenu

--- Alternate Start/README.md
+++ Alternate Start/README.md
@@ -3,31 +3,31 @@ Allows the player to select a location to spawn at when their character is first
 
 To Install:
 
-1a. Download alternateStart.lua and put it in your scripts\menu folder
+1. Download alternateStart.lua and put it in your scripts\menu folder
 
-1b. Download altStartHelper.lua and put it in your scripts folder
+2. Download altStartHelper.lua and put it in your scripts folder
 
-2. Go to config.lua, find config.menuHelperFiles and add "alternateStart" to the list
+3. Go to config.lua, find config.menuHelperFiles and add "alternateStart" to the list
 
-3a. In eventHandler.lua, find the eventHandler.OnPlayerEndCharGen function and just before the end add the following two lines
+4. In eventHandler.lua, find the eventHandler.OnPlayerEndCharGen function and just before the end add the following two lines
 ```lua
 Players[pid].currentCustomMenu = "Alternate Start"
 menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)
 ```
 
-3b. In logicHandler.lua, at the top of the file paste the following with the other require statements
+5. In logicHandler.lua, at the top of the file paste the following with the other require statements
 ```lua
 altStartHelper = require("altStartHelper")
 ```
 
-3c. Then also in logicHandler.lua, paste the following block near the bottom, before the return statements
+6. Then also in logicHandler.lua, paste the following block near the bottom, before the return statements
 ```lua
 logicHandler.TeleportPlayerToLocation = function(pid, altCell,altPos,altRot)
 	altStartHelper.altStartFunction(pid, altCell,altPos,altRot)
 end
 ```
 
-4. In Scripts\Players\base.lua Comment or Delete the following block from BasePlayer:EndCharGen
+7. In Scripts\Players\base.lua Comment or Delete the following block from BasePlayer:EndCharGen
 NOTE: This step can be skipped if you're unsure, players will just load into balmora before the alternate start menu pops up
 
 ```lua
---
+++ Res Menu/resMenuHelper.lua
@@ -0,0 +1,66 @@
+require("config")
+require("enumerations")
+
+local resMenuHelper = {}
+
+
+resMenuHelper.ResMenuFunction = function(pid, location)
+
+    local currentResurrectType
+	if location == 1 then
+	    currentResurrectType = enumerations.resurrect.TRIBUNAL_TEMPLE
+	else
+        currentResurrectType = enumerations.resurrect.IMPERIAL_SHRINE
+	end
+
+
+    local message = "You have been revived"
+
+    if currentResurrectType == enumerations.resurrect.IMPERIAL_SHRINE then
+        message = message .. " at the nearest Imperial shrine"
+    elseif currentResurrectType == enumerations.resurrect.TRIBUNAL_TEMPLE then
+        message = message .. " at the nearest Tribunal temple"
+    end
+
+    message = message .. ".\n"
+
+    -- Ensure that dying as a werewolf turns you back into your normal form
+    if Players[pid].data.shapeshift.isWerewolf == true then
+        Players[pid]:SetWerewolfState(false)
+    end
+
+    -- Ensure that we unequip deadly items when applicable, to prevent an
+    -- infinite death loop
+    contentFixer.UnequipDeadlyItems(pid)
+
+    tes3mp.Resurrect(pid, currentResurrectType)
+
+    if config.deathPenaltyJailDays > 0 then
+        local resurrectionText = "You've been revived and brought back here, " ..
+            "but your skills have been affected by "
+        local jailTime = config.deathPenaltyJailDays
+
+        if config.bountyResetOnDeath == true then
+            if config.bountyDeathPenalty == true then
+                local currentBounty = tes3mp.GetBounty(pid)
+
+                if currentBounty > 0 then
+                    jailTime = jailTime + math.floor(currentBounty / 100)
+                    resurrectionText = resurrectionText .. "your bounty and "
+                end
+            end
+
+            tes3mp.SetBounty(pid, 0)
+            tes3mp.SendBounty(pid)
+            Players[pid]:SaveBounty()
+        end
+
+        resurrectionText = resurrectionText .. "your time spent incapacitated.\n"
+        tes3mp.Jail(pid, jailTime, true, true, "Recovering", resurrectionText)
+    end
+
+    tes3mp.SendMessage(pid, message, false)
+end
+
+
+return resMenuHelper
