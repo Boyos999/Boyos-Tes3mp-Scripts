ca747a5f9d10fe90e7415272e416c30806017395
Boyos999 <Boyos999@gmail.com>
2019-01-19 15:40:32 -0500

    Ragefire scripts
    added a few customized ragefire scripts at snapjaw's request

---
+++ Ragefire/dungeonLoot.lua
@@ -0,0 +1,104 @@
+local dungeonLoot = {}
+
+local jsonInterface = require("jsonInterface")
+local ragefireTieredItem = require("ragefireTieredItem")
+--Remove this if you don't care about random items
+
+
+local jsondata = nil
+
+--The Id of the chest is extremely important, it determines what loot table is used
+--ex. dungeonchest_armor_one will look for the jsonfile "armorone" in the data folder
+--additional letter can be added after ex. dungeonchest_armor_one_a, the additional
+--letter are not taken into account but will have seperate cooldowns
+--If in different Cells, they will ahve different cooldowns regardless
+
+--You will either need to enable places object collision, or set the collision overide on for each id
+--If you don't they won't have collision
+
+--Time in seconds between valid loots
+--Default is 1 hour
+local cooldownTime = 3600
+
+function dungeonLoot.CheckId(objectRefId, pid)
+	local splitObjectRefId = objectRefId:split("_")
+	if splitObjectRefId[1] == "dungeonchest" then
+		return true
+	else
+		return false
+	end
+end
+
+function dungeonLoot.CheckCooldown(pid, locationName, chestId)
+	local state
+	local playerName = Players[pid].name
+	jsondata = jsonInterface.load("DungeonLoot.json")
+	if jsondata == nil then
+		jsondata = {}
+	end
+	
+	if jsondata[playerName] == nil then
+		jsondata[playerName] = {}
+	end
+	
+	if jsondata[playerName][locationName] == nil then
+		jsondata[playerName][locationName] = {}
+	end
+	
+	if jsondata[playerName][locationName][chestId] == nil then
+		jsondata[playerName][locationName][chestId] = {}
+	end
+	
+	if jsondata[playerName][locationName][chestId].loottime == nil then
+		jsondata[playerName][locationName][chestId].loottime = os.time()
+		state = true
+	elseif os.time() <= (jsondata[playerName][locationName][chestId].loottime+cooldownTime) then
+		state = false
+		tes3mp.MessageBox(pid, -1, "The chest is empty")
+	else 
+		jsondata[playerName][locationName][chestId].loottime = os.time()
+		state = true
+	end
+	dungeonLoot.SaveJson(jsondata)
+	
+	return state
+end
+
+function dungeonLoot.SaveJson(jsondata)
+	jsonInterface.save("DungeonLoot.json", jsondata)
+end
+
+function dungeonLoot.Reward(pid, objectRefId)
+	local splitObjectRefId = objectRefId:split("_")
+	local lootTableName = splitObjectRefId[2] .. splitObjectRefId[3]
+	local lootTable = jsonInterface.load(lootTableName .. ".json")
+	local length = table.getn(lootTable)
+	local lootRoll = math.random(1,length)
+	local itemId = lootTable[lootRoll]
+	
+	--This is where you handle cases where the table entry isn't an itemId (ex. randomItems)
+	--I recommend using a prefix in the id (ex. by_<whatever>) then using the Id to determine what to do with it
+	--this example uses the id by_randomarmor_2 to make a random piece of armor with 2 enchants
+	local splitItemId = itemId:split("_")
+	
+	if splitItemId[1] == "ragefire" then
+		--You'll need an if statement for each non-standard id
+		local itemType = splitItemId[2]
+		local itemTier = splitItemId[3]
+		itemId = ragefireTieredItem.BaseFunction(pid,1)
+		inventoryHelper.addItem(Players[pid].data.inventory,itemId,1)
+		
+	else
+	--otherwise just add the Id to the player's inventory
+		inventoryHelper.addItem(Players[pid].data.inventory,itemId,1)
+	end
+	--local packetItem = {refid = itemId, count = 1}
+	--Players[pid]:LoadItemChanges({packetItem}, enumerations.inventory.ADD)
+	Players[pid]:LoadInventory()
+	Players[pid]:LoadEquipment()
+	Players[pid]:Save()
+	tes3mp.MessageBox(pid, -1, "You find an item within the chest.")
+	
+end
+
+return dungeonLoot
---
+++ Ragefire/ragefireTieredItem.lua
@@ -0,0 +1,260 @@
+local ragefireTieredItem = {}
+
+jsonInterface = require("jsonInterface")
+
+
+ragefireTieredItem.BaseFunction = function(pid, tier)
+	local itemType = math.random(1,3)
+	local itemId
+	if itemType == 1 then
+		itemId = ragefireTieredItem.Weapon(pid,tier)
+	end
+	if itemType == 2 then
+		itemId = ragefireTieredItem.Armor(pid,tier)
+	end
+	if itemType == 3 then
+		itemId = ragefireTieredItem.Clothing(pid,tier)
+	end
+	return itemId
+end
+
+ragefireTieredItem.Weapon = function(pid, tier)
+	local weaponType = math.random(1,6)
+	local weaponRand
+	local weaponBaseId
+	local weaponName
+	local weaponBaseRand
+	local enchantmentId
+	local idIterator = WorldInstance.data.customVariables.ragefireTieredItemCounter
+	local weaponId
+	local recordTable = {}
+	local weaponTable
+	local enchTier
+	if tier == 1 then
+		weaponTable = jsonInterface.load("tierOneWeapons.json")
+		enchTier = 1
+	end
+	if weaponType == 1 then
+		weaponBaseRand = math.random(1,table.getn(weaponTable.axes))
+		weaponBaseId = weaponTable.axes[weaponBaseRand].refid
+		weaponName = weaponTable.axes[weaponBaseRand].name
+	elseif weaponType == 2 then
+		weaponBaseRand = math.random(1,table.getn(weaponTable.marksman))
+		weaponBaseId = weaponTable.marksman[weaponBaseRand].refid
+		weaponName = weaponTable.marksman[weaponBaseRand].name
+	elseif weaponType == 3 then
+		weaponBaseRand = math.random(1,table.getn(weaponTable.Blunt))
+		weaponBaseId = weaponTable.Blunt[weaponBaseRand].refid
+		weaponName = weaponTable.Blunt[weaponBaseRand].name
+	elseif weaponType == 4 then
+		weaponBaseRand = math.random(1,table.getn(weaponTable.longblade))
+		weaponBaseId = weaponTable.longblade[weaponBaseRand].refid
+		weaponName = weaponTable.longblade[weaponBaseRand].name
+	elseif weaponType == 5 then
+		weaponBaseRand = math.random(1,table.getn(weaponTable.shortblade))
+		weaponBaseId = weaponTable.shortblade[weaponBaseRand].refid
+		weaponName = weaponTable.shortblade[weaponBaseRand].name
+	else
+		weaponBaseRand = math.random(1,table.getn(weaponTable.spear))
+		weaponBaseId = weaponTable.spear[weaponBaseRand].refid
+		weaponName = weaponTable.spear[weaponBaseRand].name
+	end
+	
+	--add enchantment
+	enchantmentId = ragefireTieredItem.Enchantment(pid, enchTier)
+	
+	--make Id Unique
+	if WorldInstance.data.customVariables.ragefireTieredItemCounter == nil then
+		idIterator = 0
+	else
+		idIterator = idIterator + 1
+	end
+	WorldInstance.data.customVariables.ragefireTieredItemCounter = idIterator
+	weaponId = string.lower("TieredItem_" .. idIterator)
+	
+	--Build record table
+	recordTable["baseId"] = weaponBaseId
+	recordTable["name"] = weaponName .. " of " .. enchantmentId.name
+	recordTable["enchantmentId"] = enchantmentId.id
+	
+	ragefireTieredItem.CreateRecord(pid, weaponId, "weapon", recordTable)
+	return weaponId
+end
+
+ragefireTieredItem.Armor = function(pid, tier)
+	--variables
+	local armorTypeRand = math.random(1,3)
+	local armorBaseId
+	local armorName
+	local armorBaseRand
+	local enchantmentId
+	local armorId
+	local idIterator = WorldInstance.data.customVariables.ragefireTieredItemCounter
+	local armorTable
+	local recordTable = {}
+	local enchTier
+	
+	--Define Tier
+	if tier == 1 then
+		armorTable = jsonInterface.load("tierOneArmor.json")
+		enchTier = 1
+	end
+	
+	--Determine Armor
+	if armorTypeRand == 1 then
+		armorBaseRand = math.random(1,table.getn(armorTable.light))
+		armorBaseId = armorTable.light[armorBaseRand].refid
+		armorName = armorTable.light[armorBaseRand].name
+	elseif armorTypeRand == 2 then
+		armorBaseRand = math.random(1,table.getn(armorTable.medium))
+		armorBaseId = armorTable.medium[armorBaseRand].refid
+		armorName = armorTable.medium[armorBaseRand].name
+	else
+		armorBaseRand = math.random(1,table.getn(armorTable.heavy))
+		armorBaseId = armorTable.heavy[armorBaseRand].refid
+		armorName = armorTable.heavy[armorBaseRand].name
+	end
+	
+	--add enchantment
+	enchantmentId = ragefireTieredItem.Enchantment(pid, enchTier)
+	
+	--make Id Unique
+	if WorldInstance.data.customVariables.ragefireTieredItemCounter == nil then
+		idIterator = 0
+	else
+		idIterator = idIterator + 1
+	end
+	WorldInstance.data.customVariables.ragefireTieredItemCounter = idIterator
+	armorId = string.lower("TieredItem_" .. idIterator)
+	
+	--Build record table
+	recordTable["baseId"] = armorBaseId
+	recordTable["name"] = armorName .. " of " .. enchantmentId.name
+	recordTable["enchantmentId"] = enchantmentId.id
+	
+	ragefireTieredItem.CreateRecord(pid, armorId, "armor", recordTable)
+	return armorId
+end
+
+ragefireTieredItem.Clothing = function(pid, tier)
+	local recordTable = {}
+	local idIterator = WorldInstance.data.customVariables.ragefireTieredItemCounter
+	local clothingBaseId
+	local clothingName
+	local clothingBaseRand
+	local enchantmentId
+	local clothingId
+	local clothingTable
+	local enchTier
+	if tier == 1 then
+		clothingTable = jsonInterface.load("tierOneClothing.json")
+		enchTier = 1
+	end
+	
+	clothingBaseRand = math.random(1,table.getn(clothingTable.clothing))
+	clothingBaseId = clothingTable.clothing[clothingBaseRand].refid
+	clothingName = clothingTable.clothing[clothingBaseRand].name
+	
+	--add enchantment
+	enchantmentId = ragefireTieredItem.Enchantment(pid, enchTier)
+	
+	--make Id Unique
+	if WorldInstance.data.customVariables.ragefireTieredItemCounter == nil then
+		idIterator = 0
+	else
+		idIterator = idIterator + 1
+	end
+	WorldInstance.data.customVariables.ragefireTieredItemCounter = idIterator
+	clothingId = string.lower("TieredItem_" .. idIterator)
+	
+	--Build record table
+	recordTable["baseId"] = clothingBaseId
+	recordTable["name"] = clothingName .. " of " .. enchantmentId.name
+	recordTable["enchantmentId"] = enchantmentId.id
+	
+	ragefireTieredItem.CreateRecord(pid, clothingId, "clothing", recordTable)
+	return clothingId
+	
+end
+
+ragefireTieredItem.Enchantment = function(pid, tier)
+	local skillIdList = {4,5,6,7,10,11,12,13,14,15,18,19,20,22,23,25,26}
+	local effectRand
+	local effectName
+	local recordTable = {}
+	local effectIndex
+	local effectDur = 1
+	local effectAoe = 1
+	local effectRange = 0
+	local effectCost = 0
+	local effectCharge = 0
+	local effectMag
+	local effect
+	local effectSkill = -1
+	local effectAttr = -1
+	local enchantmentId
+	local returnTable = {}
+	local enchantmentTable
+	local idIterator = WorldInstance.data.customVariables.ragefireTieredEnchCounter
+	if tier == 1 then
+		enchantmentTable = jsonInterface.load("tierOneCE.json")
+	end
+	effectRand = math.random(1,table.getn(enchantmentTable.Constant))
+	effect = enchantmentTable.Constant[effectRand]
+	effectIndex = effect.EnchIndex
+	effectName = effect.EnchName
+	effectMag = math.random(effect.EnchMagMin,effect.EnchMagMax)
+	if effectIndex == 83 then
+		effectSkill = skillIdList[math.random(1,17)]
+	elseif effectIndex == 79 then
+		effectAttr = math.random(0,7)
+	end
+	
+	if WorldInstance.data.customVariables.ragefireTieredEnchCounter == nil then
+		idIterator = 0
+	else
+		idIterator = idIterator + 1
+	end
+	WorldInstance.data.customVariables.ragefireTieredEnchCounter = idIterator
+	enchantmentId = string.lower("TieredEnchant_" .. idIterator)
+	
+	recordTable["subtype"] = 3
+	recordTable["cost"] = effectCost
+	recordTable["charge"] = effectCharge
+	if recordTable.effects == nil then
+		recordTable.effects = {}
+	end
+	local nuEffect = { id = effectIndex, rangeType = effectRange,
+                        duration = effectDur, area = effectAoe,
+                        magnitudeMin = effectMag, magnitudeMax = effectMag,
+                        attribute = effectAttr, skill = effectSkill }
+    table.insert(recordTable.effects, nuEffect)
+	
+	ragefireTieredItem.CreateRecord(pid, enchantmentId, "enchantment", recordTable)
+	returnTable.id = enchantmentId
+	returnTable.name = effectName
+	
+	return returnTable
+	
+end
+
+ragefireTieredItem.CreateRecord = function(pid, itemId, recordType, recordTable)
+	local id = itemId
+	local recordStore = RecordStores[recordType]
+	local savedTable = recordTable
+	
+	recordStore.data.permanentRecords[id] = savedTable
+	recordStore:Save()
+    tes3mp.ClearRecords()
+    tes3mp.SetRecordType(enumerations.recordType[string.upper(recordType)])
+	
+	if recordType == "armor" then packetBuilder.AddArmorRecord(id, savedTable)
+	elseif recordType == "clothing" then packetBuilder.AddClothingRecord(id, savedTable)
+	elseif recordType == "weapon" then packetBuilder.AddWeaponRecord(id, savedTable)
+	elseif recordType == "enchantment" then packetBuilder.AddEnchantmentRecord(id, savedTable)
+	end
+	tes3mp.SendRecordDynamic(pid, true, false)
+end
+
+
+return ragefireTieredItem
