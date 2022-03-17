3cf7d77571ab003b047b7042feff0635ce52513b
Boyos999 <boyos999@gmail.com>
2022-03-17 17:40:33 -0500

    update potion limiter to use use 0.8.0 features

--- Potion Limiter/potionLimiter.lua
+++ Potion Limiter/potionLimiter.lua
@@ -4,7 +4,6 @@ local vanillaPotionTable = {}
 vanillaPotionTable = jsonInterface.load("custom/vanillapotions.json")
 
 --config
-local BaseMaxActivePotions = 3
 local potionLimitTable = {
     {alch = 0, potions = 3},
     {alch = 50, potions = 4},
@@ -12,11 +11,15 @@ local potionLimitTable = {
     {alch = 100, potions = 6}
 }
 
-
 function potionLimiter.OnPlayerItemUseValidator(eventStatus, pid, itemRefId)
     local isPotion = false
-    local maxActivePotions = BaseMaxActivePotions
-    
+    local maxActivePotions = potionLimitTable[1].potions
+    local activePotions = 0
+
+    if Players[pid].data.customVariables.activePotions ~= nil then
+        activePotions = Players[pid].data.customVariables.activePotions
+    end
+
     --Set the appropriate potion max
     for _,limit in pairs(potionLimitTable) do
         if tes3mp.GetSkillBase(pid,16) >= limit.alch then
@@ -28,11 +31,11 @@ function potionLimiter.OnPlayerItemUseValidator(eventStatus, pid, itemRefId)
     isPotion = potionLimiter.getIsPotion(itemRefId)
     
     --If the item used is a potion and the player can/cannot use it
-    if isPotion == true and Players[pid].data.customVariables.activePotions < maxActivePotions then
+    if isPotion == true and activePotions < maxActivePotions then
         return customEventHooks.makeEventStatus(nil,nil)
-    elseif isPotion == true and Players[pid].data.customVariables.activePotions >= maxActivePotions then
+    elseif isPotion == true and activePotions >= maxActivePotions then
         tes3mp.MessageBox(pid, -1, color.Red .. "You cannot drink more potions at this time")
-        return customEventHooks.makeEventStatus(false,false)
+        return customEventHooks.makeEventStatus(false,nil)
     end
     --The item used was not a potion
     return customEventHooks.makeEventStatus(nil,nil)
@@ -52,82 +55,31 @@ function potionLimiter.getIsPotion(itemRefId)
     return isPotion
 end
 
-function potionLimiter.getPotionDuration(itemRefId)
-    local recordStore = RecordStores["potion"]
-    if recordStore.data.generatedRecords[itemRefId] ~= nil then
-        local potionRecord = recordStore.data.generatedRecords[itemRefId]
-        potionDuration = potionLimiter.getRecordStorePotionDuration(itemRefId,potionRecord)
+--Track potions based on active effects
+function potionLimiter.OnPlayerSpellsActiveHandler(eventStatus,pid,playerPacket)
+    if eventStatus.validCustomHandlers ~= false and eventStatus.validDefaultHandler ~= false then
         
-    elseif recordStore.data.permanentRecords[itemRefId] ~= nil then
-        local potionRecord = recordStore.data.permanentRecords[itemRefId]
-        potionDuration = potionLimiter.getRecordStorePotionDuration(itemRefId,potionRecord)
-        
-    elseif vanillaPotionTable[itemRefId] ~= nil then
-        potionDuration = potionLimiter.getVanillaPotionDuration(itemRefId)
-    end
-    
-    return potionDuration
-end
+        local action = playerPacket.action
+        local spells = playerPacket.spellsActive
+        local activePotions = 0
 
-function potionLimiter.getVanillaPotionDuration(itemRefId)
-    local potionDuration = 0
-    if vanillaPotionTable[itemRefId].duration ~= nil then
-        potionDuration = vanillaPotionTable[itemRefId].duration
-    end
-    return potionDuration
-end
+        if Players[pid].data.customVariables.activePotions ~= nil then
+            activePotions = Players[pid].data.customVariables.activePotions
+        end
 
-function potionLimiter.getRecordStorePotionDuration(itemRefId,potionRecord)
-    local potionDuration = 0
-    --If the potion has a baseId and does not overwrite effects
-    if potionRecord.baseId ~= nil and potionRecord.effects == nil then
-        potionDuration = potionLimiter.getVanillaPotionDuration(potionRecord.baseId)
-    --Otherwise find the longest effect duration
-    elseif potionRecord.effects ~= nil then
-        for _,effect in pairs(potionRecord.effects) do
-            if effect.duration > potionDuration then
-                potionDuration = effect.duration
-            end
-        end 
-    end
-    return potionDuration
-end
-
-function potionLimiter.OnPlayerItemUseHandler(eventStatus, pid, itemRefId)
-    local potionDuration
-    local isPotion = potionLimiter.getIsPotion(itemRefId)
-    if isPotion == true then
-        --Both event statuses need to be ~false, since if default is false the player can't use the item
-        if eventStatus.validCustomHandlers ~= false and eventStatus.validDefaultHandler ~= false then
-            potionDuration = potionLimiter.getPotionDuration(itemRefId)
-            --don't make timers for instant potions
-            if potionDuration > 1 then
-                Players[pid].data.customVariables.activePotions = Players[pid].data.customVariables.activePotions + 1
-                local potTimer = tes3mp.CreateTimerEx("potionLimiterTimer",1000 * potionDuration,"i", pid)
-                tes3mp.StartTimer(potTimer)
+        for spellId,spellTable in pairs(spells) do
+            if potionLimiter.getIsPotion(spellId) then
+                if action == enumerations.spellbook.SET or action == enumerations.spellbook.ADD then
+                    activePotions = activePotions + 1
+                elseif action == enumerations.spellbook.REMOVE then
+                    activePotions = activePotions - 1
+                end
             end
         end
-    end
-end
 
-function potionLimiterTimer(pid)
-    --Can't do shit to players that aren't logged in (easily at least)
-    if Players[pid] ~= nil then
-        if Players[pid]:IsLoggedIn() then
-            --In case there's any funkiness you can't get negative active potions
-            if Players[pid].data.customVariables.activePotions > 0 then
-                Players[pid].data.customVariables.activePotions = Players[pid].data.customVariables.activePotions - 1
-                tes3mp.MessageBox(pid, -1, "The effects of a potion have worn off")
-            end
-        end
+        Players[pid].data.customVariables.activePotions = activePotions
     end
-end
-
---Reset on login in the case of players logging out before timers end
-function potionLimiter.resetActivePotions(eventStatus, pid)
-    Players[pid].data.customVariables.activePotions = 0
 end
 
 customEventHooks.registerValidator("OnPlayerItemUse",potionLimiter.OnPlayerItemUseValidator)
-customEventHooks.registerHandler("OnPlayerItemUse",potionLimiter.OnPlayerItemUseHandler)
-customEventHooks.registerHandler("OnPlayerAuthentified",potionLimiter.resetActivePotions)
+customEventHooks.registerHandler("OnPlayerSpellsActive",potionLimiter.OnPlayerSpellsActiveHandler)
