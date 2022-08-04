aae28f23b851b77dbde72ef24be61b756d6065b7
Boyos999 <boyos999@gmail.com>
2022-08-03 20:50:54 -0500

    add limits to potion tweaks

--- Potion Tweaks/potionConfig.lua
+++ Potion Tweaks/potionConfig.lua
@@ -6,6 +6,30 @@ potionConfig.weightMult = 1
 potionConfig.magMult = 1
 potionConfig.valueMult = 1
 
+--Used to set hard limits on properties of player made potions
+--See commented config below for format
+potionConfig.limits = {}
+--[[
+potionConfig.limits = {
+    duration = {
+        max = 180,
+        min = 10
+    },
+    magnitude = {
+        max = 50,
+        min = 1
+    },
+    weight = {
+        max = 1,
+        min = 0.1
+    },
+    value = {
+        max = 100,
+        min = 0
+    }
+}
+]]
+
 --Can specify weight and value for all player made potions
 potionConfig.overrides = {
     weight = 0.1,
@@ -13,6 +37,8 @@ potionConfig.overrides = {
 }
 
 --Used to customize magnitude and duration multipliers on a per-effect basis
+--Can set limits per-effect, see commented code in RESTORE_MAGICKA effect
+--for reference
 potionConfig.effects = {
     RESTORE_HEALTH = {
         durationMult = 0.5,
@@ -20,7 +46,18 @@ potionConfig.effects = {
     },
     RESTORE_MAGICKA = {
         durationMult = 0.5,
-        magMult = 2
+        magMult = 2,
+        --[[
+        limits = {
+            magnitude = {
+                max = 1000,
+                min = 20
+            },
+            duration = {
+                max = 4,
+                min = 1
+            }
+        }]]
     },
     FORTIFY_ATTRIBUTE = {
         durationMult = 2,
--- Potion Tweaks/potionTweaks.lua
+++ Potion Tweaks/potionTweaks.lua
@@ -3,39 +3,88 @@ require("custom.potionConfig")
 local potionTweaks = {}
 
 function potionTweaks.getValue(baseValue)
+    local value
     if potionConfig.overrides.value ~= nil then
-        return potionConfig.overrides.value
+        value = potionConfig.overrides.value
     else
-        return baseValue*potionConfig.valueMult
+        value = baseValue*potionConfig.valueMult
     end
+    return potionTweaks.checkLimits("value",value)
 end
 
 function potionTweaks.getWeight(baseWeight)
+    local weight
     if potionConfig.overrides.weight ~= nil then
-        return potionConfig.overrides.weight
+        weight = potionConfig.overrides.weight
     else
-        return baseWeight*potionConfig.weightMult
+        weight = baseWeight*potionConfig.weightMult
     end
+    return potionTweaks.checkLimits("weight",weight)
 end
 
 function potionTweaks.getMagnitude(effect, enum)
+    local mag
     if potionConfig.effects[enum] ~= nil then
         if potionConfig.effects[enum].magMult ~= nil then
-            return math.floor(effect.magnitudeMax*potionConfig.effects[enum].magMult)
+            mag = math.floor(effect.magnitudeMax*potionConfig.effects[enum].magMult)
         end
-    else
-        return math.floor(effect.magnitudeMax*potionConfig.magMult)
     end
+    if mag == nil then
+        mag = math.floor(effect.magnitudeMax*potionConfig.magMult)
+    end
+    return potionTweaks.checkLimits("magnitude",mag,enum)
 end
 
 function potionTweaks.getDuration(effect, enum)
+    local duration
     if potionConfig.effects[enum] ~= nil then
         if potionConfig.effects[enum].durationMult ~= nil then
-            return math.floor(effect.duration*potionConfig.effects[enum].durationMult)
+            duration = math.floor(effect.duration*potionConfig.effects[enum].durationMult)
         end
-    else
-        return math.floor(effect.duration*potionConfig.durationMult)
     end
+    if duration == nil then
+        duration = math.floor(effect.duration*potionConfig.durationMult)
+    end
+    return potionTweaks.checkLimits("duration",duration,enum)
+end
+
+function potionTweaks.checkLimits(property, value, enum)
+    local limits = {}
+
+    if potionConfig.limits[property] ~= nil then
+        if potionConfig.limits[property].max ~= nil then
+            limits.max = potionConfig.limits[property].max
+        end
+        if potionConfig.limits[property].min ~= nil then
+            limits.min = potionConfig.limits[property].min
+        end
+    end
+    if property == "magnitude" or property == "duration" then
+        if enum ~= nil and potionConfig.effects[enum] ~= nil then
+            if potionConfig.effects[enum].limits ~= nil then
+                if potionConfig.effects[enum].limits[property] ~= nil then
+                    if potionConfig.effects[enum].limits[property].max ~= nil then
+                        limits.max = potionConfig.effects[enum].limits[property].max
+                    end
+                    if potionConfig.effects[enum].limits[property].min ~= nil then
+                        limits.min = potionConfig.effects[enum].limits[property].min
+                    end
+                end
+            end
+        end
+    end
+
+    if limits.max ~= nil then
+        if value > limits.max then
+            return limits.max
+        end
+    end
+    if limits.min ~= nil then
+        if value < limits.min then
+            return limits.min
+        end
+    end
+    return value
 end
 
 function potionTweaks.getEnum(id)
