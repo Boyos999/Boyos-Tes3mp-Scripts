2659078750970d8328183892ffb8937e01566a2f
Boyos999 <boyos999@gmail.com>
2025-05-13 16:36:48 -0500

    fix indentation

--- javelins/javelinsConfig.lua
+++ javelins/javelinsConfig.lua
@@ -123,163 +123,163 @@ javelinsConfig.baseSpears["steel spear"] = {
 }
 
 --Tamriel Data
-    javelinsConfig.baseSpears["t_ayl_regular_spear_01"] = {
-        id = "jav_t_ayleid",
-        name = "Ayleid Javelin",
-        damage = {
-            min = 8,
-            max = 23
-        },
-        flags = "0x01"
-    }
+javelinsConfig.baseSpears["t_ayl_regular_spear_01"] = {
+    id = "jav_t_ayleid",
+    name = "Ayleid Javelin",
+    damage = {
+        min = 8,
+        max = 23
+    },
+    flags = "0x01"
+}
 
-    javelinsConfig.baseSpears["t_com_farm_pitchfork_01"] = {
-        id = "jav_t_pitchfork",
-        damage = {
-            min = 1,
-            max = 5
-        },
-        flags = "0x00"
-    }
+javelinsConfig.baseSpears["t_com_farm_pitchfork_01"] = {
+    id = "jav_t_pitchfork",
+    damage = {
+        min = 1,
+        max = 5
+    },
+    flags = "0x00"
+}
 
-    javelinsConfig.baseSpears["t_dae_regular_longspear_01"] = {
-        id = "jav_t_dae_long",
-        name = "Daedric Long Javelin",
-        damage = {
-            min = 10,
-            max = 50
-        },
-        flags = "0x01"
-    }
+javelinsConfig.baseSpears["t_dae_regular_longspear_01"] = {
+    id = "jav_t_dae_long",
+    name = "Daedric Long Javelin",
+    damage = {
+        min = 10,
+        max = 50
+    },
+    flags = "0x01"
+}
 
-    javelinsConfig.baseSpears["t_dae_regular_trident_01"] = {
-        id = "jav_t_dae_tri",
-        name = "Daedric Trident Javelin",
-        damage = {
-            min = 7,
-            max = 42
-        },
-        flags = "0x01"
-    }
+javelinsConfig.baseSpears["t_dae_regular_trident_01"] = {
+    id = "jav_t_dae_tri",
+    name = "Daedric Trident Javelin",
+    damage = {
+        min = 7,
+        max = 42
+    },
+    flags = "0x01"
+}
 
-    javelinsConfig.baseSpears["t_de_aena_spear_01"] = {
-        id = "jav_t_aena",
-        name = "Aena Javelin",
-        damage = {
-            min = 6,
-            max = 15
-        },
-        flags = "0x00"
-    }
+javelinsConfig.baseSpears["t_de_aena_spear_01"] = {
+    id = "jav_t_aena",
+    name = "Aena Javelin",
+    damage = {
+        min = 6,
+        max = 15
+    },
+    flags = "0x00"
+}
 
-    javelinsConfig.baseSpears["t_de_glass_spear_01"] = {
-        id = "jav_t_glass",
-        name = "Glass Javelin",
-        damage = {
-            min = 6,
-            max = 32
-        },
-        flags = "0x01"
-    }
+javelinsConfig.baseSpears["t_de_glass_spear_01"] = {
+    id = "jav_t_glass",
+    name = "Glass Javelin",
+    damage = {
+        min = 6,
+        max = 32
+    },
+    flags = "0x01"
+}
 
-    javelinsConfig.baseSpears["t_dwe_regular_longspear_01"] = {
-        id = "jav_t_dwe_long",
-        name = "Dwarven Long Javelin",
-        damage = {
-            min = 5,
-            max = 29
-        },
-        flags = "0x01"
-    }
+javelinsConfig.baseSpears["t_dwe_regular_longspear_01"] = {
+    id = "jav_t_dwe_long",
+    name = "Dwarven Long Javelin",
+    damage = {
+        min = 5,
+        max = 29
+    },
+    flags = "0x01"
+}
 
-    javelinsConfig.baseSpears["t_imp_barrow_spear_01"] = {
-        id = "jav_t_nedic",
-        name = "Nedic Javelin",
-        damage = {
-            min = 5,
-            max = 17
-        },
-        flags = "0x00"
-    }
+javelinsConfig.baseSpears["t_imp_barrow_spear_01"] = {
+    id = "jav_t_nedic",
+    name = "Nedic Javelin",
+    damage = {
+        min = 5,
+        max = 17
+    },
+    flags = "0x00"
+}
 
-    javelinsConfig.baseSpears["t_imp_reman_spear_01"] = {
-        id = "jav_t_reman",
-        name = "Reman Javelin",
-        damage = {
-            min = 7,
-            max = 15
-        },
-        flags = "0x00"
-    }
+javelinsConfig.baseSpears["t_imp_reman_spear_01"] = {
+    id = "jav_t_reman",
+    name = "Reman Javelin",
+    damage = {
+        min = 7,
+        max = 15
+    },
+    flags = "0x00"
+}
 
-    javelinsConfig.baseSpears["t_nor_iron_spear_01"] = {
-        id = "jav_t_nor_iron",
-        name = "Nordic Iron Javelin",
-        damage = {
-            min = 5,
-            max = 20
-        },
-        flags = "0x00"
-    }
+javelinsConfig.baseSpears["t_nor_iron_spear_01"] = {
+    id = "jav_t_nor_iron",
+    name = "Nordic Iron Javelin",
+    damage = {
+        min = 5,
+        max = 20
+    },
+    flags = "0x00"
+}
 
-    javelinsConfig.baseSpears["t_nor_orn_spear_01"] = {
-        id = "jav_t_nor_orn",
-        name = "Nordic Javelin",
-        damage = {
-            min = 6,
-            max = 21
-        },
-        flags = "0x00"
-    }
+javelinsConfig.baseSpears["t_nor_orn_spear_01"] = {
+    id = "jav_t_nor_orn",
+    name = "Nordic Javelin",
+    damage = {
+        min = 6,
+        max = 21
+    },
+    flags = "0x00"
+}
 
-    javelinsConfig.baseSpears["t_nor_silverspear_01"] = {
-        id = "jav_t_nor_silver",
-        name = "Nordic Silver Javelin",
-        damage = {
-            min = 2,
-            max = 35
-        },
-        flags = "0x03"
-    }
+javelinsConfig.baseSpears["t_nor_silverspear_01"] = {
+    id = "jav_t_nor_silver",
+    name = "Nordic Silver Javelin",
+    damage = {
+        min = 2,
+        max = 35
+    },
+    flags = "0x03"
+}
 
-    javelinsConfig.baseSpears["t_nor_stalhrimspear_01"] = {
-        id = "jav_t_nor_stalhrim",
-        name = "Stalhrim Javelin",
-        damage = {
-            min = 5,
-            max = 50
-        },
-        flags = "0x01"
-    }
+javelinsConfig.baseSpears["t_nor_stalhrimspear_01"] = {
+    id = "jav_t_nor_stalhrim",
+    name = "Stalhrim Javelin",
+    damage = {
+        min = 5,
+        max = 50
+    },
+    flags = "0x01"
+}
 
-    javelinsConfig.baseSpears["t_nor_steel_spear_01"] = {
-        id = "jav_t_nor_steel",
-        name = "Nordic Steel Javelin",
-        damage = {
-            min = 6,
-            max = 26
-        },
-        flags = "0x00"
-    }
+javelinsConfig.baseSpears["t_nor_steel_spear_01"] = {
+    id = "jav_t_nor_steel",
+    name = "Nordic Steel Javelin",
+    damage = {
+        min = 6,
+        max = 26
+    },
+    flags = "0x00"
+}
 
-    javelinsConfig.baseSpears["t_orc_regular_spear_01"] = {
-        id = "jav_t_nor_orc_01",
-        name = "Orcish Javelin",
-        damage = {
-            min = 8,
-            max = 24
-        },
-        flags = "0x01"
-    }
+javelinsConfig.baseSpears["t_orc_regular_spear_01"] = {
+    id = "jav_t_nor_orc_01",
+    name = "Orcish Javelin",
+    damage = {
+        min = 8,
+        max = 24
+    },
+    flags = "0x01"
+}
 
-    javelinsConfig.baseSpears["t_orc_regular_spear_02"] = {
-        id = "jav_t_nor_orc_02",
-        name = "Orcish Javelin",
-        damage = {
-            min = 8,
-            max = 24
-        },
-        flags = "0x01"
-    }
+javelinsConfig.baseSpears["t_orc_regular_spear_02"] = {
+    id = "jav_t_nor_orc_02",
+    name = "Orcish Javelin",
+    damage = {
+        min = 8,
+        max = 24
+    },
+    flags = "0x01"
+}
 
 return javelinsConfig
