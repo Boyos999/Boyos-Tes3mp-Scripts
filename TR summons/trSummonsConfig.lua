2277ba22e64065b444d17692e49fdff20d6693a8
Boyos999 <boyos999@gmail.com>
2025-05-13 16:32:18 -0500

    add new tamriel data mwse summons

--- TR summons/trSummonsConfig.lua
+++ TR summons/trSummonsConfig.lua
@@ -32,7 +32,19 @@ trSummonsConfig.summonMap = {
     t_dwe_cnj_uni_summonsteamcent = "centurion_steam",
     t_dwe_cnj_uni_summonspidercent = "centurion_spider",
     t_ayl_cnj_summonwelkyndspirit = "t_ayl_cre_welkspr_01",
-    t_com_cnj_summonauroran = "t_dae_cre_auroran_01"
+    t_com_cnj_summonauroran = "t_dae_cre_auroran_01",
+    t_com_cnj_summonherne = "t_dae_cre_herne_01",
+    t_com_cnj_summonmorphoid = "t_dae_cre_morphoid_01",
+    t_nor_cnj_summondraugr = "t_sky_und_drgr_01",
+    t_nor_cnj_summonspriggan = "t_sky_cre_spriggan_01",
+    t_de_cnj_summongreaterbonelord = "t_mw_und_boneldgr_01",
+    t_cyr_cnj_summonghost = "t_cyr_und_ghst_01",
+    t_cyr_cnj_summonwraith = "t_cyr_und_wrth_01",
+    t_cyr_cnj_summonbarrowguard = "t_cyr_und_mum_01",
+    t_cyr_cnj_summonminobarrowguard = "t_cyr_und_minobarrow_01",
+    t_com_cnj_summonskeletonchamp = "t_glb_und_skelcmpgls_01",
+    t_com_cnj_summonfrostmonarch = "t_dae_cre_monarchfr_01",
+    t_com_cnj_summonspiderdaedra = "t_dae_cre_spiderdae_01",
 }
 
 trSummonsConfig.summonRecords = {
@@ -40,7 +52,7 @@ trSummonsConfig.summonRecords = {
         name = "Summon Devourer",
         subtype = trSummonsConfig.subtype,
         flags = trSummonsConfig.flags,
-        cost = 155,
+        cost = 156,
         effects = trSummonsConfig.placeholderEffects
     },
     t_com_cnj_summondremoraarcher = {
@@ -61,14 +73,23 @@ trSummonsConfig.summonRecords = {
         name = "Summon Guardian",
         subtype = trSummonsConfig.subtype,
         flags = trSummonsConfig.flags,
-        cost = 207,
-        effects = trSummonsConfig.placeholderEffects
+        cost = 155,
+        effects = {{
+            id = 118, --command creature
+            rangeType = 0, --self
+            area = 0,
+            duration = 45,
+            magnitudeMax = 1000,
+            magnitudeMin = 1000,
+            attribute = -1,
+            skill = -1
+        }}
     },
     t_com_cnj_summonlesserclannfear = {
         name = "Summon Rock Chisel Clannfear",
         subtype = trSummonsConfig.subtype,
         flags = trSummonsConfig.flags,
-        cost = 66,
+        cost = 57,
         effects = trSummonsConfig.placeholderEffects
     },
     t_com_cnj_summonogrim = {
@@ -89,8 +110,17 @@ trSummonsConfig.summonRecords = {
         name = "Summon Dark Seducer",
         subtype = trSummonsConfig.subtype,
         flags = trSummonsConfig.flags,
-        cost = 225,
-        effects = trSummonsConfig.placeholderEffects
+        cost = 169,
+        effects = {{
+            id = 118, --command creature
+            rangeType = 0, --self
+            area = 0,
+            duration = 45,
+            magnitudeMax = 1000,
+            magnitudeMin = 1000,
+            attribute = -1,
+            skill = -1
+        }}
     },
     t_com_cnj_summonvermai = {
         name = "Summon Vermai",
@@ -103,14 +133,14 @@ trSummonsConfig.summonRecords = {
         name = "Summon Storm Monarch",
         subtype = trSummonsConfig.subtype,
         flags = trSummonsConfig.flags,
-        cost = 179,
+        cost = 180,
         effects = trSummonsConfig.placeholderEffects
     },
     t_nor_cnj_summonicewraith = {
         name = "Summon Ice Wraith",
         subtype = trSummonsConfig.subtype,
         flags = trSummonsConfig.flags,
-        cost = 104,
+        cost = 105,
         effects = trSummonsConfig.placeholderEffects
     },
     t_dwe_cnj_uni_summondwespectre = {
@@ -131,7 +161,7 @@ trSummonsConfig.summonRecords = {
         name = "Summon Dwemer Spider Centurion",
         subtype = trSummonsConfig.subtype,
         flags = trSummonsConfig.flags,
-        cost = 46,
+        cost = 45,
         effects = trSummonsConfig.placeholderEffects
     },
     t_ayl_cnj_summonwelkyndspirit = {
@@ -145,7 +175,100 @@ trSummonsConfig.summonRecords = {
         name = "Summon Auroran",
         subtype = trSummonsConfig.subtype,
         flags = trSummonsConfig.flags,
-        cost = 130,
+        cost = 138,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_com_cnj_summonherne = {
+        name = "Summon Herne",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 54,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_com_cnj_summonmorphoid = {
+        name = "Summon Morphoid",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 63,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_nor_cnj_summondraugr = {
+        name = "Summon Draugr",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 78,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_nor_cnj_summonspriggan = {
+        name = "Summon Spriggan",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 144,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_de_cnj_summongreaterbonelord = {
+        name = "Summon Bonelord Warder",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 160,
+        effects = {{
+            id = 118, --command creature
+            rangeType = 0, --self
+            area = 0,
+            duration = 45,
+            magnitudeMax = 1000,
+            magnitudeMin = 1000,
+            attribute = -1,
+            skill = -1
+        }}
+    },
+    t_cyr_cnj_summonghost = {
+        name = "Summon Ghost",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 21,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_cyr_cnj_summonwraith = {
+        name = "Summon Wraith",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 147,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_cyr_cnj_summonbarrowguard = {
+        name = "Summon Barrowguard",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 33,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_cyr_cnj_summonminobarrowguard = {
+        name = "Summon Minotaur Barrowguard",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 171,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_com_cnj_summonskeletonchamp = {
+        name = "Summon Skeleton Champion",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 96,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_com_cnj_summonfrostmonarch = {
+        name = "Summon Frost Monarch",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 141,
+        effects = trSummonsConfig.placeholderEffects
+    },
+    t_com_cnj_summonspiderdaedra = {
+        name = "Summon Spider Daedra",
+        subtype = trSummonsConfig.subtype,
+        flags = trSummonsConfig.flags,
+        cost = 126,
         effects = trSummonsConfig.placeholderEffects
     }
 }
